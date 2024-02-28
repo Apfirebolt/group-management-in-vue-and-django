fe4eed6bce888abeba2fbe6539964edd952cbdee
Apfirebolt <aspper20@gmail.com>
2024-02-28 06:58:39 +0530

    feat: mobile menu and logout

--- client/src/components/HeaderComponent.vue
+++ client/src/components/HeaderComponent.vue
@@ -17,16 +17,15 @@
             </div>
             <div class="hidden sm:block sm:ml-6">
               <div class="flex space-x-4">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                <router-link v-for="item in navigation" :key="item.name" :to="{ name: item.name }" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</router-link>
+                <button v-if="authData" @click="logOutUtil" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Log Out
+                </button>
               </div>
             </div>
           </div>
           <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button type="button" class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
-  
+
             <!-- Profile dropdown -->
             <Menu as="div" class="ml-3 relative">
               <div>
@@ -55,21 +54,25 @@
   
       <DisclosurePanel class="sm:hidden">
         <div class="px-2 pt-2 pb-3 space-y-1">
-          <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+          <router-link v-for="item in navigation" :key="item.name" :to="{ name: item.name }" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</router-link>
+          <button @click="logOutUtil" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-mdfont-medium">
+            Log Out
+          </button>
         </div>
       </DisclosurePanel>
     </Disclosure>
   </template>
   
   <script>
+  import { computed } from 'vue'
   import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
   import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+  import { useAuth } from '../store/auth'
   
   const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
+    { name: 'Home', path: '/', current: true },
+    { name: 'Login', path: '/login', current: false },
+    { name: 'Register', path: '/register', current: false },
   ]
   
   export default {
@@ -86,8 +89,20 @@
       XIcon,
     },
     setup() {
+      const auth = useAuth()
+
+      const authData = computed(() => {
+        return auth.getAuthData
+      })
+
+      const logOutUtil = () => {
+        auth.logout()
+      }
+  
       return {
         navigation,
+        logOutUtil,
+        authData
       }
     },
   }
