dcc81eada5f3be78fafd6a002a517b61a25df632
Apfirebolt <aspper20@gmail.com>
2024-03-02 08:21:34 +0530

    feat: test cases for suppliers

--- api/tests/test_supplier_api.py
+++ api/tests/test_supplier_api.py
@@ -15,6 +15,18 @@ CREATE_SUPPLIER_URL = reverse('api:create-supplier')
 LIST_SUPPLIERS_URL = reverse('api:list-suppliers')
 
 
+def detail_supplier_url(pk):
+    """Create and return a Supplier detail URL."""
+    return reverse('api:supplier-detail', args=[pk])
+
+
+def create_supplier(**params):
+    """Create and return a sample Supplier."""
+
+    supplier = Supplier.objects.create(**params)
+    return supplier
+
+
 class PublicSupplierApiTests(TestCase):
     """Test the public features of the user API."""
 
@@ -35,6 +47,49 @@ class PublicSupplierApiTests(TestCase):
         res = self.client.get(LIST_SUPPLIERS_URL)
         
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+    def test_delete_supplier_success(self):
+        """Test deleting a question is successful."""
+        supplier = create_supplier(
+            name='Test Supplier',
+        )
+        res = self.client.delete(detail_supplier_url(supplier.id))
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        supplier_exists = Supplier.objects.filter(
+            name=supplier.name
+        ).exists()
+        self.assertFalse(supplier_exists)
+
+    def test_update_supplier(self):
+        """Test updating a supplier."""
+        supplier = create_supplier(
+            name='Test Supplier',
+        )
+        payload = {'name': 'Updated Supplier'}
+        res = self.client.patch(detail_supplier_url(supplier.id), payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        supplier.refresh_from_db()
+        self.assertEqual(supplier.name, payload['name'])
+    
+    def test_update_supplier_not_found(self):
+        """Test updating a supplier that does not exist."""
+        payload = {'name': 'Updated Supplier'}
+        res = self.client.patch(detail_supplier_url(1), payload)
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_get_single_supplier(self):
+        """Test getting a single supplier."""
+        supplier = create_supplier(
+            name='Test Supplier',
+        )
+        res = self.client.get(detail_supplier_url(supplier.id))
+        serializer = SupplierSerializer(res.data)
+        data = serializer.data
+        self.assertTrue('name' in data)
+        self.assertTrue('id' in data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data['name'], supplier.name)
 
 
     
