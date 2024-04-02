3a251df873ab12760306d373429e7998c42e4703
Apfirebolt <aspper20@gmail.com>
2024-04-02 22:49:36 +0530

    read me changes

--- groups/admin.py
+++ groups/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from . models import Group, GroupQueue, GroupTask
 
+
 admin.site.register(Group)
 admin.site.register(GroupQueue)
 admin.site.register(GroupTask)
--- items/admin.py
+++ items/admin.py
@@ -2,6 +2,30 @@ from django.contrib import admin
 from . models import Category, Supplier, Item
 
 
-admin.site.register(Category)
-admin.site.register(Supplier)
-admin.site.register(Item)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'updated_at', 'is_active']
+    search_fields = ['name',]
+    list_filter = ['name',]
+    list_per_page = 10
+    ordering = ['name', 'created_at']
+
+
+class AdminSupplier(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'updated_at', 'is_active']
+    search_fields = ['name',]
+    list_filter = ['name',]
+    list_per_page = 10
+    ordering = ['name', 'created_at']
+
+
+class AdminItem(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'updated_at', 'is_active']
+    search_fields = ['name',]
+    list_filter = ['name',]
+    list_per_page = 10
+    ordering = ['name', 'created_at']
+
+
+admin.site.register(Category, AdminCategory)
+admin.site.register(Supplier, AdminSupplier)
+admin.site.register(Item, AdminItem)
